1c7f190691a92be418a3f8c92f1a4588d72472dd
James Burnett <HurricaneJamesEsq@gmail.com>
2023-10-19 15:26:34 -0700

    add doc comment for removeDuplicatesFromSortedArray

--- src/removeDuplicatesFromSortedArray.ts
+++ src/removeDuplicatesFromSortedArray.ts
@@ -12,15 +12,23 @@
 //   nums are not important as well as the size of nums.
 // * Return k.
 
-// time: O(n) / space: O(1)
+/**
+ * In place remove duplicates from a sorted array. The elements will remain in relative
+ * order. Unique elements will be shifted to the first k elements of the array, and k
+ * will be returned. Elements after k are undefined and should be ignored.
+ * @complexity time: O(n) / space: O(1)
+ *
+ * @param nums an array of numbers sorted in non-decreasing order
+ * @returns the number of unique elements in nums
+ */
 export default function removeDuplicates(nums: number[]): number {
   let current = nums[0];
   let k = 1;
-  for (let i=1; i<nums.length; i++) {
+  for (let i = 1; i < nums.length; i++) {
     if (nums[i] !== current) {
       nums[k++] = nums[i];
       current = nums[i];
     }
   }
   return k;
-};
+}
