318c1d833a8f90ed3fadfae385e2bf9f4b83a198
James Burnett <HurricaneJamesEsq@gmail.com>
2023-10-21 18:57:41 -0700

    add missing complexity analysis

--- src/wordSearch.ts
+++ src/wordSearch.ts
@@ -19,18 +19,24 @@
 // Constraints:
 // * `m == board.length`
 // * `n = board[i].length`
+// * `l === word.length`
 // * `1 <= m, n <= 6`
 // * `1 <= word.length <= 15`
 // * `board` and `word` consists of only lowercase and uppercase English letters.
 //
 // Follow-up Questions:
 // * could you use search pruning to make your solution faster with a larger `board`?
-
+//
+// Complexity Analysis:
+// O(m * n * 4^l) time complexity, O(m * n + l) space complexity
 export default function exist(board: string[][], word: string): boolean {
   const rows = board.length;
   const cols = board[0].length;
+
+  // O(m * n) time/space complexity
   const visited: boolean[][] = new Array(rows).fill(0).map(() => new Array(cols).fill(false));
 
+  // O(4^l) time complexity, O(l) space complexity
   function dfs(row: number, col: number, index: number): boolean {
     if (row < 0 || row >= rows || col < 0 || col >= cols) return false;
     if (board[row][col] !== word[index]) return false;
@@ -39,6 +45,7 @@ export default function exist(board: string[][], word: string): boolean {
     visited[row][col] = true;
     const nextIdx = index + 1;
 
+    // O(4^l) time complexity, O(l) space complexity
     const result =
       nextIdx === word.length ||
       dfs(row + 1, col, nextIdx) ||
@@ -51,6 +58,7 @@ export default function exist(board: string[][], word: string): boolean {
     return false;
   }
 
+  // O(m * n * 4^l) time complexity, O(l) space complexity
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       // dfs from board[row][col]
